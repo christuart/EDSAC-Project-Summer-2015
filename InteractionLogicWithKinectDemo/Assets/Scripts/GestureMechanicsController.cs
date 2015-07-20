35b1a508de9c484c9b247b9abb42cd8eb8e6143a
christuart <cis24@cam.ac.uk>
2015-07-20 16:27:35 +0100

    Added ability to point up-right

--- InteractionLogicWithKinectDemo/Assets/Scripts/GestureMechanicsController.cs
+++ InteractionLogicWithKinectDemo/Assets/Scripts/GestureMechanicsController.cs
@@ -7,25 +7,31 @@ public class GestureMechanicsController : MonoBehaviour {
 	public Text trackedJointsText;
 
 	private KinectManager km;
-	private KinectWrapper.NuiSkeletonPositionIndex rightHand = KinectWrapper.NuiSkeletonPositionIndex.HandRight;
+	private KinectWrapper.NuiSkeletonPositionIndex rightHand = KinectWrapper.NuiSkeletonPositionIndex.HandLeft; // remember, body is flipped as facing away from kinect
 	private KinectWrapper.NuiSkeletonPositionIndex centreHip = KinectWrapper.NuiSkeletonPositionIndex.HipCenter;
 	private KinectWrapper.NuiSkeletonPositionIndex centreShoulder = KinectWrapper.NuiSkeletonPositionIndex.ShoulderCenter;
-	private KinectWrapper.NuiSkeletonPositionIndex rightShoulder = KinectWrapper.NuiSkeletonPositionIndex.ShoulderRight;
-	private KinectWrapper.NuiSkeletonPositionIndex leftShoulder = KinectWrapper.NuiSkeletonPositionIndex.ShoulderLeft;
-	private KinectWrapper.NuiSkeletonPositionIndex rightWrist = KinectWrapper.NuiSkeletonPositionIndex.WristRight;
+	private KinectWrapper.NuiSkeletonPositionIndex rightShoulder = KinectWrapper.NuiSkeletonPositionIndex.ShoulderLeft;
+	private KinectWrapper.NuiSkeletonPositionIndex leftShoulder = KinectWrapper.NuiSkeletonPositionIndex.ShoulderRight;
+	private KinectWrapper.NuiSkeletonPositionIndex rightWrist = KinectWrapper.NuiSkeletonPositionIndex.WristLeft;
 	
-	// Use this for initialization
-	void Awake () {
-		km = KinectManager.Instance;
+	private Vector3 wristVector;
+	private Vector3 shouldersVector;
+	private Vector3 backVector;
+
+	void Start() {
+		wristVector = new Vector3 (0.1f, -1f, 0f);
+		shouldersVector = new Vector3 (1f, 0f, 0f);
+		backVector = new Vector3 (0f, 1f, 0f);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		km = KinectManager.Instance;
 		string trackedJointsString = "Tracked Joints:";
 		if (km != null) {
 			if (km.IsUserDetected()) {
-				Debug.Log ("checking joints");
 				uint userId = km.GetPlayer1ID();
+				/*trackedJointsString += "\nchecking joints";
 				if(km.IsJointTracked(userId, (int)rightHand))
 					trackedJointsString += "\nRight hand";
 				if(km.IsJointTracked(userId, (int)rightWrist))
@@ -38,24 +44,26 @@ public class GestureMechanicsController : MonoBehaviour {
 					trackedJointsString += "\nCentre shoulder";
 				if(km.IsJointTracked(userId, (int)rightShoulder))
 					trackedJointsString += "\nRight shoulder";
-				Debug.Log ("cone checking joints");
+				trackedJointsString += "\ndone checking joints";*/
 
-				
-				Vector3 wristVector = km.GetRawSkeletonJointPos(userId, (int)rightWrist) - km.GetRawSkeletonJointPos(userId, (int)rightShoulder);
-				Vector3 shouldersVector = km.GetRawSkeletonJointPos(userId, (int)rightShoulder) - km.GetRawSkeletonJointPos(userId, (int)leftShoulder);
-				Vector3 backVector = km.GetRawSkeletonJointPos(userId, (int)centreShoulder) - km.GetRawSkeletonJointPos(userId, (int)centreHip);
+				if (km.IsJointTracked(userId, (int)rightHand) && km.IsJointTracked(userId, (int)rightShoulder))
+					wristVector = km.GetJointPosition(userId, (int)rightHand) - km.GetJointPosition(userId, (int)rightShoulder);
+				if (km.IsJointTracked(userId, (int)rightShoulder) && km.IsJointTracked(userId, (int)leftShoulder))
+					shouldersVector = km.GetJointPosition(userId, (int)rightShoulder) - km.GetJointPosition(userId, (int)leftShoulder);
+				if (km.IsJointTracked(userId, (int)centreShoulder) && km.IsJointTracked(userId, (int)centreHip))
+					backVector = km.GetJointPosition(userId, (int)centreShoulder) - km.GetJointPosition(userId, (int)centreHip);
 
 				//GramSchmidt Orthonormal Space
-				Vector3 e1 = shouldersVector.normalized;
-				Vector3 e2 = (backVector - Vector3.Dot (backVector,e1) * e1).normalized;
+				Vector3 e2 = backVector.normalized;
+				Vector3 e1 = (shouldersVector - Vector3.Dot (shouldersVector,e2) * e2).normalized;
 
 				Vector2 wristVectorInPlane = new Vector2(Vector3.Dot(e1, wristVector), Vector3.Dot(e2, wristVector));
 				if (wristVectorInPlane.y > 0 && wristVectorInPlane.y > (wristVectorInPlane.x / 2f))
 					trackedJointsString += "\nPointing up right: " + Mathf.Rad2Deg * Mathf.Atan2 (wristVectorInPlane.y,wristVectorInPlane.x) + "degrees";
 
 			}
-			trackedJointsText.text = trackedJointsString;
 		}
+		trackedJointsText.text = trackedJointsString;
 	}
 
 }
